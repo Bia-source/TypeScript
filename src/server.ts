2c55191e5a5c450ff7e9fb772d8b3543881883b7
Beatriz Ferreira dos Santos <bfsantos@RDM503728.local>
2021-04-03 20:27:28 -0300

    configuração finalizada

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { request } from 'express';
 import { createCourse } from './routes';
 
 const app = express();
 
 app.get("/", createCourse);
+app.post("/",(request, response)=>{
+    return response.json({message: "hello "});
+})
 
 app.listen(1899);
